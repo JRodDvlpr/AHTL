a47a36b0d6460aa1b464408b9c1dbbb0535505fe
JRodDvlpr <Mialienproductions@gmail.com>
2021-11-03 13:11:08 -0400

    Header/Search Component

--- ahtl/pages/index.js
+++ ahtl/pages/index.js
@@ -1,14 +1,18 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title></title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Building a Hotel </h1>
+      {/** TODO:  Header */}
+      <Header />
+      {/** TODO:  Banner */}
+
       
     </div>
   )
