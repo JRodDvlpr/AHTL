1d3a2b62b0b24cd03098be911a65b6cc2062c014
JRodDvlpr <Mialienproductions@gmail.com>
2021-11-15 12:14:34 -0500

    Footer Completed

--- ahtl/components/Footer.js
+++ ahtl/components/Footer.js
@@ -2,8 +2,41 @@
 
 function Footer() {
     return (
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+            <div className='space-y-4 text-gray-800'>
+            <h5 className='font-bold'>ABOUT</h5>
+            <p>How AHTL Works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>AHTL Plus</p>
+            <p>AHTL Luxe</p>
+            </div>
+            <div className='space-y-4 text-gray-800'>
+            <h5 className='font-bold'>COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is a project site</p>
+            <p>Testing my skills</p>
+            <p>Referrals Accepted</p>
+            <p>Jesus Rodriguez</p>
+            </div>
+            <div className='space-y-4 text-gray-800'>
+            <h5 className='font-bold'>HOST</h5>
+            <p>Jesus Rodriguez React</p>
+            <p>Presents</p>
+            <p>AHTL</p>
+            <p>Join Now</p>
+            <p>Hundreds of Homes </p>
+            </div>
+            <div className='space-y-4 text-gray-800'>
+            <h5 className='font-bold'>SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Contact Us</p>
+            <p>Location</p>
+            <p>Support Forms</p>
+            </div>
             
+
         </div>
     )
 }
