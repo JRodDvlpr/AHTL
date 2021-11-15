10c07ab9bbe920ecb1abb1171e1b535db2b7897b
JRodDvlpr <Mialienproductions@gmail.com>
2021-11-15 12:00:20 -0500

    Medium & Large Card Done

--- ahtl/pages/index.js
+++ ahtl/pages/index.js
@@ -3,6 +3,8 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import MediumCard from '../components/MediumCard'
 import SmCard from '../components/SmCard'
+import LargeCard from '../components/LargeCard'
+import Footer from '../components/Footer'
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -31,7 +33,7 @@ export default function Home({ exploreData, cardsData }) {
 
           <section>
             <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-            <div className='flex space-x-3 overflow-scroll scrollbar-hide'>
+            <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
             {cardsData?.map(({ img, title }) => (
               <MediumCard 
               key={img}  
@@ -44,7 +46,16 @@ export default function Home({ exploreData, cardsData }) {
           </section>
           
         </section>
+
+        <LargeCard 
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Wishlists curated by AHTL'
+          buttonText='Get Inspired'
+        />
       </main>
+
+      <Footer />
 
     </div>
   )
